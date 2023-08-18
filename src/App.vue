d167d2c746996c29dbc886cb01833041974e24d1
nathanaelsanilo <nathanaelsanilo@gmail.com>
2023-08-18 23:01:50 +0700

    build: add husky

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const state = ref({
+  name: '',
+  age: 0,
+  active: false
+})
+</script>
 
 <template>
-  <main class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">hello world</main>
+  <main class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+    {{ state.active }}
+  </main>
 </template>
 
 <style scoped></style>
