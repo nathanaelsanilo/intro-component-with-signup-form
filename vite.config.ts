5e0f3bbd397639216c2d6b4a5d311674dbca14b0
nathanaelsanilo <nathanaelsanilo@gmail.com>
2023-08-20 18:22:42 +0700

    fix: setup base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/intro-component-with-signup-form/',
   server: {
     port: 3000
   },
